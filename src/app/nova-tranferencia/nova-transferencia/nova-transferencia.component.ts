dd83a157916ef52b8f8e0442104965fbfdfafa0b
JOAO GABRIEL C JATAI <951549924@adm.unifor.br@DTEC65046811.unifor.br>
2022-10-06 17:33:39 -0300

    histórico das  transferências

--- src/app/nova-tranferencia/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-tranferencia/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
 
-  constructor() { }
+  valor: number;
+  destino: number;
 
-  ngOnInit(): void {
+  transferir() {
+    console.log('Transferência solicitada');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
   }
 
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+  ngOnInit(): void {}
 }
